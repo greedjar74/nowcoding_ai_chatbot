b695964ec32501ae810d272570437d6b9cf4e633
unknown <hong7498kim@naver.com>
2025-05-14 15:35:32 +0900

    .

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main_page():
     st.title('나우코딩랩스 생성형 AI 실습')
     st.write('각 stage에 대한 실습을 진행하세요.')
 
-page_names_to_funcs = {'Main Page': main_page, 'MZ': MZ_main, 'NP': NP_main}
+page_names_to_funcs = {'Main Page': main_page, 'MZ 실습': MZ_main, 'NP 실습': NP_main}
 
 selected_page = st.sidebar.selectbox('Select a stage', page_names_to_funcs.keys())
 
